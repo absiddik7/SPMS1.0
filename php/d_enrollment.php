42ed37b33c6bb60eda339b6933f003c1902e423a
Adnan Rahman <arahman4946@gmail.com>
2021-09-09 22:46:50 +0600

    Updated by Adnan

--- php/d_enrollment.php
+++ php/d_enrollment.php
@@ -23,14 +23,4 @@
 
         $progs = $conn->query($query);
     }
-
-    $query = "SELECT COUNT(DISTINCT(enrollment.student_id)) as 'total' FROM school LEFT JOIN department ON school.id = department.school_id LEFT JOIN program ON department.id = program.department_id LEFT JOIN course ON program.id = course.program_id LEFT JOIN section ON course.id = section.course_id LEFT JOIN enrollment ON section.id = enrollment.section_id WHERE school.id=$school_id";
-    $enrolled = $conn->query($query)->fetch_row()[0];
-
-    $query = "SELECT COUNT(program.id) as 'total' FROM school LEFT JOIN department ON school.id = department.school_id LEFT JOIN program ON department.id = program.department_id WHERE school.id = $school_id";
-    $prog_count = $conn->query($query)->fetch_row()[0];
-
-    $query = "SELECT COUNT(course.id) as 'total' FROM school LEFT JOIN department ON school.id = department.school_id LEFT JOIN program ON department.id = program.department_id LEFT JOIN course ON program.id = course.program_id WHERE school.id=$school_id";
-    $crs_count = $conn->query($query)->fetch_row()[0];
-
 ?>
