2432140ef5d2f75bdad6be554e31615fdb8077c6
AB Siddik <abubakkar1070@gmail.com>
2021-09-11 16:32:14 +0600

    Edited by Abubakkar
    Sections List

---
+++ admin/sections-list.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Sections List | Admin</title>
+
+    <link rel="stylesheet" href="../assets/vendors/iconfonts/mdi/css/materialdesignicons.css">
+    <link rel="stylesheet" href="../assets/vendors/css/vendor.addons.css">
+    <link rel="stylesheet" href="../assets/vendors/datatables/jquery.dataTables.css">
+    <link rel="stylesheet" href="../assets/css/shared/style.css">>
+    <link rel="stylesheet" href="../assets/css/demo_1/style.css">
+
+  </head>
+  <body class="header-fixed">
+    <nav class="t-header">
+      <div class="t-header-brand-wrapper">
+        <a href="index.html">
+          <img class="logo" src="../assets/images/logo.png" alt="">
+          <img class="logo-mini" src="../assets/images/logo_mini.png" alt="">
+        </a>
+      </div>
+      <div class="t-header-content-wrapper">
+        <div class="t-header-content">
+        <h4>Sections List</h4>
+          <button class="t-header-toggler t-header-mobile-toggler d-block d-lg-none">
+            <i class="mdi mdi-menu"></i>
+          </button>
+        </div>
+      </div>
+    </nav>
+    <div class="page-body">
+      <div class="sidebar">
+        <div class="user-profile">
+          <div class="display-avatar">
+            <img class="profile-img img-lg rounded-circle" src="../assets/images/profile-pic.png" alt="profile image">
+          </div>
+          <div class="info-wrapper">
+            <h4 class="user-name"></h4>
+          </div>
+        </div>
+        <ul class="navigation-menu">
+          <li>
+            <a style="font-size:medium;" href="dashboard.php">
+              <span class="link-title">Dashboard</span>
+              <i class="mdi mdi-gauge link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a style="font-size:medium;" href="users-list.php">
+              <span class="link-title">Users</span>
+              <i class="mdi mdi-account-multiple link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a style="font-size:medium;" href="schools-list.php">
+              <span class="link-title">Schools</span>
+              <i class="mdi mdi-animation link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a style="font-size:medium;" href="departments-list.php">
+              <span class="link-title">Departments</span>
+              <i class="mdi mdi-archive link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a style="font-size:medium;" href="programs-list.php">
+              <span class="link-title">Programs</span>
+              <i class="mdi mdi-clipboard-outline link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a style="font-size:medium;" href="courses-list.php">
+              <span class="link-title">Courses</span>
+              <i class="mdi mdi-library-books link-icon"></i>
+            </a>
+          </li>
+          <li class="active">
+            <a style="font-size:medium;" href="sections-list.php">
+              <span class="link-title">Sections</span>
+              <i class="mdi mdi-code-equal link-icon"></i>
+            </a>
+          </li>
+          <li>
+            <a style="font-size:medium;" href="../php/login.php?logout=1">
+              <span class="link-title">Logout</span>
+              <i class="mdi mdi-logout link-icon"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="page-content-wrapper">
+        <div class="page-content-wrapper-inner">
+          <div class="content-viewport">
+            <div class="row">
+              <div class="col-9 py-1">
+              <div class="col-1 py-5 text-right">
+                <a class="btn btn-success has-icon" href="section-create.php">
+                  <i class="mdi mdi-account-plus-outline"></i>Add
+                </a>
+              </div>
+            </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="grid">
+                <div class="item-wrapper">
+                  <div class="table-responsive">
+                    <table class="table display" id="user-table">
+                      <thead>
+                        <tr>
+                          <th>Sl</th>
+                          <th>Section No</th>
+                          <th>Semester</th>
+                          <th>Course ID</th>
+                          <th>Faculty Name</th>
+                          <th>Total Students</th>
+                        </tr>
+                      </thead>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer class="footer">
+          <div class="row">
+            <div class="col-sm-6 text-center text-sm-right order-sm-1">
+              <ul class="text-gray">
+                <li><a href="#">Terms of use</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-6 text-center text-sm-left mt-3 mt-sm-0">
+              <small class="text-muted d-block">Copyright © 2021 <a href="#" >Coder Buddies</a>. All rights reserved</small>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  </body>
+</html>
